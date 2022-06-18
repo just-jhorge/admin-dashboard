0e487bd891aaa09024f5e9819b13099ffbcb6014
just-jhorge <sarpongk247@gmail.com>
2022-06-18 08:10:02 +0000

    Sidebar edited

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ import { links } from "../data/dummy";
 const Sidebar = () => {
     const activeMenu = true;
     const activeLink =
-        "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+        "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white bg-red-400 text-md m-2";
     const normalLink =
         "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
